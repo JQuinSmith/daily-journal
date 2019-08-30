23288676b5d583904ab97da174adede05e45f119
QuinWithOneN <jquinsmyth@gmail.com>
2019-08-30 18:21:38 -0500

    major style updates

---
+++ src/scripts/journal.js
@@ -0,0 +1,25 @@
+
+const journalEntry = []
+journalEntry.push({
+   dateOfEntry: "August 19th",
+   conceptsCovered: "no class-lab day",
+   moodOfTheDay: "sunny",
+   entry: "worked on the daily journal page with css. I also added css to the govtrack page.",
+})
+
+journalEntry.push({
+   dateOfEntry: "August 20th",
+   conceptsCovered: "C33 Demo Day",
+   moodOfTheDay: "cloudy",
+   entry: "We watched C33 Demos. That afternoon we were split into group and started on group projects.",
+})
+
+journalEntry.push({
+   dateOfEntry: "August 21st",
+   conceptsCovered: "Group Project",
+   moodOfTheDay: "foggy",
+   entry: "We worked on our group project and decided how to layout our webpage. We picked an artist that we wanted to make a tribute page for.",
+})
+
+
+console.log("journalEntry", journalEntry);
