4fe98d03e1488a2eaefdb54d407e21d7437db72e
QuinWithOneN <jquinsmyth@gmail.com>
2019-09-03 11:00:09 -0500

    huge style updates, added functions to post entry objects to the DOM

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,25 +1,44 @@
 
-const journalEntry = []
-journalEntry.push({
-   dateOfEntry: "August 19th",
-   conceptsCovered: "no class-lab day",
-   moodOfTheDay: "sunny",
-   entry: "worked on the daily journal page with css. I also added css to the govtrack page.",
-})
+const journalEntries = [
+   {
+      dateOfEntry: "August 19th",
+      conceptsCovered: "no class-lab day",
+      moodOfTheDay: "Sunny",
+      entry: "Worked on the daily journal page with css. I also added css to the govtrack page.",
+   },
+   {
+      dateOfEntry: "August 20th",
+      conceptsCovered: "C33 Demo Day",
+      moodOfTheDay: "peaceful",
+      entry: "We watched C33 Demos. That afternoon we were split into group and started on group projects.",
+   },
+   {
+      dateOfEntry: "August 21st",
+      conceptsCovered: "Group Project",
+      moodOfTheDay: "Pumped",
+      entry: "We worked on our group project and decided how to layout our webpage. We picked an artist that we wanted to make a tribute page for."
+   }
+]
 
-journalEntry.push({
-   dateOfEntry: "August 20th",
-   conceptsCovered: "C33 Demo Day",
-   moodOfTheDay: "cloudy",
-   entry: "We watched C33 Demos. That afternoon we were split into group and started on group projects.",
-})
+const makeJournalEntryComponent = (journalEntries) => {
+   // Create your own HTML structure for a journal entry
+   return `
+      <div class="segment">
+         <h3>Date of Entry:</h3>
+         <p>${journalEntries.dateOfEntry}</p>
+         <h3>Concepts Covered:</h3>
+         <p>${journalEntries.conceptsCovered}</p>
+         <h3>Mood of the Day:</h3>
+         <p>${journalEntries.moodOfTheDay}</p>
+         <h3>Entry:</h3>
+         <p>${journalEntries.entry}</p>
+      </div>
+   `
+}
 
-journalEntry.push({
-   dateOfEntry: "August 21st",
-   conceptsCovered: "Group Project",
-   moodOfTheDay: "foggy",
-   entry: "We worked on our group project and decided how to layout our webpage. We picked an artist that we wanted to make a tribute page for.",
-})
+const entryContainer = document.querySelector(".entryLog")
 
+ journalEntries.forEach( entries => {
+   entryContainer.innerHTML += makeJournalEntryComponent(entries);
+});
 
-console.log("journalEntry", journalEntry);
