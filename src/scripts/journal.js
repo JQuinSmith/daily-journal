81b265062e12123215d2aec810b6324afd644790
QuinWithOneN <jquinsmyth@gmail.com>
2019-09-13 15:41:57 -0500

    character validation working

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -10,8 +10,36 @@ import addEntriesToDOM from "./entryComponent.js"
 
 API.getJournalEntries().then(addEntriesToDOM)
 
-     
+// Event listener for the 'create new entry' button
+document.querySelector("#submit-button").addEventListener("click", () => {
 
+    // 1. needs to get values of the inputs/dropdowns
+    // they're stored in variables to use later
+    const journalDate = document.querySelector("#dateId").value
+    const journalConcept = document.querySelector("#conceptId").value
+    const journalMood = document.querySelector("#moodId").value
+    const journalEntry = document.querySelector("#entryId").value
+ 
+console.log(journalDate);
+console.log(journalConcept);
+console.log(journalMood);
+console.log(journalEntry);
 
+    if (journalDate === "" || journalConcept === "" || journalMood === "" || journalEntry === "") {
+        alert("Missing an input!")
+    }
+    if (journalConcept.includes("@", "#", "$", "%", "^") || journalEntry.includes("@", "#", "$", "%", "^")){
+        alert("No special characters!");
+    }
+})
 
+const makeJournalEntry = (date, concept, mood, entry) => {
+    const newJournalEntry = {
+        date: date,
+        concept: concept,
+        mood: mood,
+        entry: entry,
+    }
+    return newJournalEntry
+}
 
